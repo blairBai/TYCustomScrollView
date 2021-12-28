c1df92677d98c5f45d92eb4c4fd559795149b904
baiyunfei <513737874@qq.com>
2021-12-28 18:07:13 +0800

    change repo address

---
+++ TYCustomScrollView/Classes/TYNormalScrollView.h
@@ -0,0 +1,16 @@
+//
+//  TYNormalScrollView.h
+//  FBSnapshotTestCase
+//
+//  Created by BYF on 12/28/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TYNormalScrollView : UIScrollView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TYCustomScrollView/Classes/TYNormalScrollView.m
@@ -0,0 +1,20 @@
+//
+//  TYNormalScrollView.m
+//  FBSnapshotTestCase
+//
+//  Created by BYF on 12/28/21.
+//
+
+#import "TYNormalScrollView.h"
+
+@implementation TYNormalScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
